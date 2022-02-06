ceddc78075c98ab1330a6f3c23146bc636ec6589
yunjin-21 <96698362+yunjin-21@users.noreply.github.com>
2022-02-06 21:25:49 +0900

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,32 +1,91 @@
-package Cofffee;
-
+package cooperation;
 
 public class Person {
+	 public String PassengerName;   
+	 public int age;             
+	public int distance;
+	public int fare;
 	
-	String name;
-	int money;
-			
-	Person(String name, int money ){
-			this.name = name;
-			this.money = money;
-			} // 생성자를 만들어야 함. 이름과 돈(잔액) 생성!
-			
-			
+	 public Person(String PassengerName, int age, int distance) { // 나이와 거리 초기화하기 
+		  
+		  this.PassengerName = PassengerName;
+		  this.age = age;
+		  this.distance = distance;
+		 
+	 
+
+	 if (age<=13) {
+		 if(distance <2000) {
+
+			 fare = 450;}
+		 else {
+			 fare = 450 + 100*((distance - 2000)/500);	}			 
+			 
+	 }
+	 else if (age<20){
+		 if(distance <2000) {
+
+			 fare = 720;}
+		 else {
+			 fare = 720 + 100*((distance - 2000)/500);				 
+		 }
+	 }
+		 
+	 else if (age<=65){
+		 if(distance <2000) {
+
+			 fare = 1250;}
+		 else { 
+			 fare = 1250 + 100*((distance - 2000)/500);				 
+		 }
+	 }
+	 else  {
+		 if(distance <2000) {
+
+			 fare = 500;}
+		 else {
+			 fare = 500 + 100*((distance - 2000)/500);				 
+		 }
+	 }
 	
-	public void buyStarCoffee(StarCoffee sCoffee) {
-		sCoffee.buy(4000);
-		this.money -= 4000;
-	}
+	 
+	 } //
 	
+	 
+	 public void rideBus(Bus bus) {
+		 
+		 bus.pay(fare);
+		 }
 	
-	public void buyBeanCoffee(BeanCoffee bCoffee) {
-		bCoffee.buy(4500);
-		this.money -= 4500;
-	}
-	
-	public void showInfo() {
-		System.out.println(name + "님이 " + money +"원이 남았습니다.");
-		
-	}
-	
+	    
+	 public void showInfo() {
+		 System.out.println(PassengerName + "은 나이가 "+age+ "살이고 버스에서 " +distance + "m의 거리를 이동하였습니다.");
+	 }
+
+
 }
+
+/* bus 기본 요금
+어린이: 450
+청소년: 720
+성인: 1250
+경로우대: 500
+
+이동 거리에 따라서 가격을 다르게 
+-> bus
+기본 요금: 2km
+추가: 500m 당 100원 씩 */
+
+
+/* money(fare) 변수를 생성
+메서드 생성 distance >2000 경우
+money = fare; 
+메서드 2개
+1.fare값 대입
+2. 거리>2000일때 식 만들기
+
+  
+  */
+
+
+
